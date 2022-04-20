56d261dc401ac6981a1dfe3ba4a4f6cb63fc3218
iispace <junjinyoung14@gmail.com>
2022-04-20 18:48:57 +0900

    Update Write json.py

--- Read Coco Format Annotations/Write json.py
+++ Read Coco Format Annotations/Write json.py
@@ -1,3 +1,15 @@
+label_file = r".\labels.txt"
+all_class_labels = [[1],[1],[1],[1],[1],[1],[2],[2],[2],[2],[3],[3],[3],[3],[3],[3]]
+all_transformed_bboxes = [[[110, 122, 311, 166]],
+                         [[110, 122, 311, 166]],[[110, 122, 311, 166]],[[110, 122, 311, 166]],
+                         [[110, 122, 311, 166]],[[110, 122, 311, 166]],[[110, 122, 311, 166]],
+                         [[110, 122, 311, 166]],[[110, 122, 311, 166]],[[110, 122, 311, 166]],
+                         [[110, 122, 311, 166]],[[110, 122, 311, 166]],[[110, 122, 311, 166]],
+                         [[110, 122, 311, 166]],[[110, 122, 311, 166]],[[110, 122, 311, 166]]]
+
+now = datetime.datetime.now()
+now_str = now.strftime("%Y-%m-%d")
+        
 data = dict(
         info=dict(
         url=None,
@@ -19,21 +31,25 @@ data = dict(
         ],
     )
     
-before = data['categories']
-print('Before appending any data:')
-print(json.dumps(before, indent=4)
-print("\n")
+for i, (bboxes, class_labels) in enumerate(zip(all_transformed_bboxes, all_class_labels)):
+    annot_dic = dict(segmentation=None, area=None, iscrowd=0, image_id=i, bbox=bboxes, category_id= class_labels)
+    data['annotations'].append(annot_dic)
+    img_dic = dict(license=None, url=None, file_name=f"{i}.jpg", height=height, width=width, date_captured=now_str, id=i)
+    data['images'].append(img_dic)
+      
 
+for i, (bboxes, class_labels) in enumerate(zip(all_transformed_bboxes, all_class_labels)):
+    dic = dict(segmentation=None, area=None, iscrowd=0, image_id=i, bbox=bboxes, category_id= class_labels)
+    data['annotations'].append(dic)
+      
 with open(label_file, 'r') as f:
     for i, line in enumerate(f.readlines()):
         class_id = i - 1
         class_name = line.strip()
-        #print(f'LINE [{i}] : ', class_name)
         if class_id == -1:
             assert class_name == '__ignore__'
             continue
-        #print(f'LINE [{i}] : ', class_name)
         class_name_to_id[class_name] = class_id
         data['categories'].append(dict(supercategory=None, id=class_id, name=class_name))
-print("After appending data:")
-print(json.dumps(data['categories'], indent=4))
+
+
