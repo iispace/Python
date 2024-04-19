ba2d4dc8fcb8c6b0663b186ca866e3ab4969e15e
iispace <junjinyoung14@gmail.com>
2024-04-19 17:24:15 +0900

    Update ViT_OrangeQ_Dataloader.py

--- VisionTransformer/ViT_OrangeQ_Dataloader.py
+++ VisionTransformer/ViT_OrangeQ_Dataloader.py
@@ -8,11 +8,12 @@ from torchvision.transforms import transforms
 from torch.utils.data import WeightedRandomSampler, DataLoader
 
 class ImgPatcherNDataLoader:
-    def __init__(self, dataname='imagenet', patch_size=16, img_size=256, batch_size=64):
+    def __init__(self, IMAGE_ROOT=None, dataname='OrangeQuality', patch_size=16, img_size=256, batch_size=64):
         self.patch_size = patch_size 
         self.dataname = dataname 
         self.img_size = img_size
         self.batch_size = batch_size 
+        self.IMAGE_ROOT = IMAGE_ROOT # 이미지 파일이 있는 최상위 경로 (예: D:\OrangeQuality)
 
     def make_weights(self, labels, nclasses):
         labels = np.array(labels)  # labels: numpay.ndarray([192])
@@ -32,9 +33,8 @@ class ImgPatcherNDataLoader:
         test_transform  = transforms.Compose([OrangeQ_Resize(self.img_size), OrangeQ_PatchGenerator(self.patch_size)])
         
         if self.dataname == 'OrangeQuality':
-            IMAGE_ROOT = rf"D:\HYCU_Lab\0_JJ\2_HYCU_MasterDegree\Data\ConvertedImage\OrangeQuality"
-            trainset = OrangeQualityDataset(IMAGE_ROOT, train=True, transform=train_transform)
-            testset  = OrangeQualityDataset(IMAGE_ROOT, train=False, transform=test_transform)
+            trainset = OrangeQualityDataset(self.IMAGE_ROOT, train=True, transform=train_transform)
+            testset  = OrangeQualityDataset(self.IMAGE_ROOT, train=False, transform=test_transform)
 
         elif self.dataname == 'imgagenet':
             pass 
