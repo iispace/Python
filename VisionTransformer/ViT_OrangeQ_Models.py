49001566c781077df92872fa8842d863922fadc3
iispace <junjinyoung14@gmail.com>
2024-04-19 17:47:42 +0900

    Update and rename ViT_OrangeQ_Reg_Model.py to ViT_OrangeQ_Models.py

--- VisionTransformer/ViT_OrangeQ_Models.py
+++ VisionTransformer/ViT_OrangeQ_Models.py
@@ -1,5 +1,98 @@
 import torch.nn as nn 
 
+########## ViT Classification Model ##########
+class LinearProjection(nn.Module):
+    def __init__(self, patch_vec_size, num_patches, latent_vec_dim, drop_rate):
+        super(LinearProjection, self).__init__()
+        self.linear_proj   = nn.Linear(patch_vec_size, latent_vec_dim)
+        self.cls_token     = nn.Parameter(torch.randn(1, latent_vec_dim))
+        self.pos_embedding = nn.Parameter(torch.randn(1, num_patches+1, latent_vec_dim))
+        self.dropout       = nn.Dropout(drop_rate)
+
+    def forward(self, x):
+        batch_size = x.size(0) # x: torch.tensor [Batch_size, N, C*p*p]  
+        x = x.to(self.linear_proj.weight.dtype) # x와 weight의 dtype 일치시키기 위함.
+
+        x = torch.cat([self.cls_token.repeat(batch_size, 1, 1), self.linear_proj(x)], dim=1)
+        x += self.pos_embedding 
+        x = self.dropout(x)
+        return x 
+    
+class MultiHeadSelfAttention(nn.Module):
+    def __init__(self, latent_vec_dim, num_heads, drop_rate) :
+        super(MultiHeadSelfAttention, self).__init__()
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.num_heads = num_heads 
+        self.latent_vec_dim = latent_vec_dim 
+        self.head_dim = int(latent_vec_dim / num_heads) # k 
+        self.query = nn.Linear(latent_vec_dim, latent_vec_dim)
+        self.key = nn.Linear(latent_vec_dim, latent_vec_dim)
+        self.value = nn.Linear(latent_vec_dim, latent_vec_dim)
+        self.scale = torch.sqrt(self.head_dim *  torch.ones(1)).to(device)
+        self.dropout = nn.Dropout(drop_rate)
+
+
+    def forward(self, x):
+        batch_size = x.size(0)
+        q = self.query(x)
+        k = self.key(x)
+        v = self.value(x)
+        q = q.view(batch_size, -1, self.num_heads, self.head_dim).permute(0,2,1,3)
+        k = k.view(batch_size, -1, self.num_heads, self.head_dim).permute(0,2,3,1) # k.T (Transpose of k)
+        v = v.view(batch_size, -1, self.num_heads, self.head_dim).permute(0,2,1,3)
+        attention = torch.softmax(q @ k / self.scale, dim=-1)
+        x = self.dropout(attention) @ v 
+        x = x.permute(0,2,1,3).reshape(batch_size, -1, self.latent_vec_dim) # 각 head에 대한 모든 self-attention 결과가 하나로 concat()되도록 reshape함.
+
+        return x, attention
+    
+class TFencoder(nn.Module):
+    def __init__(self, latent_vec_dim, num_heads, mlp_hidden_dim, drop_rate):
+        super(TFencoder, self).__init__()
+        self.ln1 = nn.LayerNorm(latent_vec_dim)
+        self.ln2 = nn.LayerNorm(latent_vec_dim)
+        self.msa = MultiHeadSelfAttention(latent_vec_dim=latent_vec_dim, num_heads=num_heads, drop_rate=drop_rate)
+        self.dropout = nn.Dropout(drop_rate)
+        self.mlp = nn.Sequential(nn.Linear(latent_vec_dim, mlp_hidden_dim),
+                                 nn.GELU(), nn.Dropout(drop_rate),
+                                 nn.Linear(mlp_hidden_dim, latent_vec_dim),
+                                 nn.Dropout(drop_rate))
+    def forward(self, x):
+        z = self.ln1(x)
+        z, att_map = self.msa(z)
+        z = self.dropout(z)
+        x = x + z 
+        z = self.ln2(x)
+        z = self.mlp(z)
+        x = x + z
+        return x, att_map 
+    
+class VisionTransformer(nn.Module):
+    def __init__(self, patch_vec_size, num_patches, latent_vec_dim, num_heads, mlp_hidden_dim, drop_rate, num_layers, num_classes) :
+        super(VisionTransformer, self).__init__()
+        self.patchembedding = LinearProjection(patch_vec_size=patch_vec_size, num_patches=num_patches,
+                                               latent_vec_dim=latent_vec_dim, drop_rate=drop_rate)
+        self.transformer    = nn.ModuleList([TFencoder(latent_vec_dim=latent_vec_dim, num_heads=num_heads,
+                                                            mlp_hidden_dim=mlp_hidden_dim, drop_rate=drop_rate)
+                                            for _ in range(num_layers)])
+        self.mlp_head = nn.Sequential(nn.LayerNorm(latent_vec_dim), nn.Linear(latent_vec_dim, num_classes))
+
+    def forward(self, x):
+        """  
+            x: torch.tensor [Batch_size, N, C*p*p] torch.float32
+        """
+        att_list = []
+        x = self.patchembedding(x)
+        for layer in self.transformer:
+            x, att = layer(x)
+            att_list.append(att)
+        x = self.mlp_head(x[:,0]) # class token만 추출해서 mlp_head에 보냄
+
+        return x, att_list 
+
+
+
+########## ViT Regression Model ##########
 class LinearProjection(nn.Module):
     def __init__(self, patch_vec_size, num_patches, latent_vec_dim, drop_rate):
         super(LinearProjection, self).__init__()
@@ -81,7 +174,7 @@ class VisionTransformer(nn.Module):
         self.transformer    = nn.ModuleList([TFencoder(latent_vec_dim=latent_vec_dim, num_heads=num_heads, mlp_hidden_dim=mlp_hidden_dim, drop_rate=drop_rate)
                                             for _ in range(num_layers)])
         
-        ################# regression task를 위해 mlp_head 부분을 fc로 수정
+        # regression task를 위해 mlp_head 부분을 fc로 수정
         out_feature = 1 # for regression
         self.mlp_head = nn.Sequential(nn.LayerNorm(latent_vec_dim), 
                                       nn.Linear(latent_vec_dim, 64),
