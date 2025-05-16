df1d5967ed6f1f0b195a90c71c4295e05504bc33
iispace <junjinyoung14@gmail.com>
2025-05-16 11:12:11 +0900

    Update nir_generation.py

--- RGB2NIR/nir_generation.py
+++ RGB2NIR/nir_generation.py
@@ -9,7 +9,27 @@ model_dis = Discriminator(in_channels=3).to(device)
 model_gen.apply(initialize_weights);
 model_dis.apply(initialize_weights);
 
+# 손실함수
+loss_func_gan = nn.BCELoss()
+loss_func_pix = nn.L1Loss()
 
+# loss_func_pix 가중치
+lambda_pixel = 100
+
+# patch 수
+patch = (1,256//2**4,256//2**4)
+print(f"patch size: {patch}")
+
+# 최적화 파라미터
+from torch import optim
+lr = 2e-4
+beta1 = 0.5
+beta2 = 0.999
+
+opt_dis = optim.Adam(model_dis.parameters(),lr=lr,betas=(beta1,beta2))
+opt_gen = optim.Adam(model_gen.parameters(),lr=lr,betas=(beta1,beta2))
+#opt_dis = optim.SGD(model_dis.parameters(),lr=lr)
+#opt_gen = optim.SGD(model_gen.parameters(),lr=lr)
 
 
 experiment_no = 4
