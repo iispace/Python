70d426b4e397840c0832c6312edad199e2786c61
iispace <junjinyoung14@gmail.com>
2025-05-16 11:11:52 +0900

    Update nir_generation.py

--- RGB2NIR/nir_generation.py
+++ RGB2NIR/nir_generation.py
@@ -1,12 +1,13 @@
 # Generator, Discrminator 모델 생성
-from models.Pix2Pix_RGB2NIR import *
+from models.Pix2Pix_RGB2NIR import NIR_GeneratorUNet, Discriminator, initialize_weights
 
 model_gen = NIR_GeneratorUNet(in_channels=3, out_channels=3).to(device)
 model_dis = Discriminator(in_channels=3).to(device)
 
 
-
-
+# 가중치 초기화 적용
+model_gen.apply(initialize_weights);
+model_dis.apply(initialize_weights);
 
 
 
